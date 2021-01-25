18b5d4efa0a625c4e9c0e7e5f0c401655d1c49be
marcosmro <marcos.martinez.romero@gmail.com>
2021-01-25 00:14:09 -0800

    Add leading zeros to zip codes

--- python/zipcodes_by_state.py
+++ python/zipcodes_by_state.py
@@ -11,10 +11,11 @@ def generate_zipcodes_by_state():
         zips = json.load(f)
         for zip_obj in zips:
             state_abbreviation = zip_obj['state']
-            if state_abbreviation in output:
-                output[state_abbreviation].append(zip_obj['zip_code'])
-            else:
-                output[state_abbreviation] = [zip_obj['zip_code']]
+            if not state_abbreviation in output:
+                output[state_abbreviation] = []
+
+            zip_str = str(zip_obj['zip_code']).zfill(5) # fill out to 5 chars
+            output[state_abbreviation].append(zip_str)
 
     with open('transformed_data/zipCodesByState.json', 'w', encoding='utf-8') as f:
         json.dump(output, f, indent=4)
