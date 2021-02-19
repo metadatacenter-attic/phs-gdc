c85dcb7124df9f5ad011074acf99dbb526e98a5d
marcosmro <marcos.martinez.romero@gmail.com>
2021-02-18 18:29:25 -0800

    Extract statistical variables as a list

--- python/stat-vars/statvar_scraper.py
+++ python/stat-vars/statvar_scraper.py
@@ -8,31 +8,41 @@ from bs4 import BeautifulSoup
 
 
 def get_statvars():
+    # TODO: the statistical variables page is checked-in here:
+    #  "https://raw.githubusercontent.com/datacommonsorg/website/master/static/data/hierarchy_statsvar.json".
+    #  Maybe read the hierarchy from this file in the future
     url = "https://docs.datacommons.org/statistical_variables.html"
     doc = requests.get(url).text
     soup = BeautifulSoup(doc, 'html.parser')
     main_content = soup.find(id="main-doc-content")
 
+    # Generate tree
     statvars_tree = []
     for tag in main_content.contents:
         if tag.name == 'details':
-            statvars_tree.append(parse_details(tag))
+            statvars_tree.append(parse_details_as_tree(tag))
 
-    print(statvars_tree)
-
-    with open('dc_statvars.json', 'w', encoding='utf-8') as f:
+    with open('dc_statvars_tree.json', 'w', encoding='utf-8') as f:
         json.dump(statvars_tree, f, indent=2)
 
+    # Generate list
+    statvars_list = []
+    for tag in main_content.contents:
+        if tag.name == 'details':
+            parse_details_as_list(tag, statvars_list, None)
 
-def parse_details(details):
+    with open('dc_statvars_list.json', 'w', encoding='utf-8') as f:
+        json.dump(statvars_list, f, indent=2)
 
+
+def parse_details_as_tree(details):
     node_name = details.summary.string
     node_id = node_name.lower()
     node_children = []
     if details.details:
         for tag in details.contents:
             if tag.name == 'details':
-                node_children.append(parse_details(tag))
+                node_children.append(parse_details_as_tree(tag))
     else:
         if details.ul:
             cont = details.ul.contents
@@ -50,6 +60,26 @@ def parse_details(details):
         "id": node_id,
         "children": node_children
     }
+
+
+def parse_details_as_list(details, statvar_list, parent_category):
+    category_name = details.summary.string
+    if details.details:
+        for tag in details.contents:
+            if tag.name == 'details':
+                parse_details_as_list(tag, statvar_list, category_name)
+    else:
+        if details.ul:
+            cont = details.ul.contents
+        else:
+            cont = details.contents
+        for tag in cont:
+            if tag.name == 'li':
+                statvar_list.append({
+                    "name": tag.a.string,
+                    "label": tag.a.string,
+                    "category": category_name if parent_category is None else (parent_category + ' - ' + category_name)
+                })
 
 
 def main():
