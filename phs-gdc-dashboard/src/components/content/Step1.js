ebce77498e3b9ba55f7b0c8f155069659ac35d44
Jennifer Vendetti <vendetti@stanford.edu>
2021-09-08 11:30:02 -0700

    Indent components for entering location values

--- phs-gdc-dashboard/src/components/content/Step1.js
+++ phs-gdc-dashboard/src/components/content/Step1.js
@@ -11,6 +11,7 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Radio from "@material-ui/core/Radio";
 import states from './../../resources/locationData/allStates.json';
 import Autocomplete from "@material-ui/lab/Autocomplete/Autocomplete";
+import Box from "@material-ui/core/Box";
 import Checkbox from "@material-ui/core/Checkbox/Checkbox";
 import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 import CheckBoxIcon from '@material-ui/icons/CheckBox';
@@ -28,6 +29,9 @@ export default function Step1(props) {
       marginTop: theme.spacing(4),
       textAlign: 'left',
     },
+    extraIndent: {
+      marginLeft: theme.spacing(3.5),
+    }
   }));
 
   const classes = useStyles();
@@ -101,56 +105,60 @@ export default function Step1(props) {
         <RadioGroup value={valueOptionRadio} onChange={handleChangeValuesOptionRadio}>
           {/* Option to enter location values by hand */}
           <FormControlLabel value="optionEnter" control={<Radio color={"primary"}/>} label="Enter location values by hand"/>
-          <TextField
-            fullWidth
-            id="standard-multiline-flexible"
-            label="Enter values (one per line)"
-            multiline
-            rowsMax={20}
-            rows={6}
-            value={variableValues}
-            variant="outlined"
-            disabled={valueOptionRadio === "optionSelect"}
-            onChange={handleChangeValuesField}
-            onBlur={e => {
-              handleChangeValuesField(e);
-              props.validateStep1VariableValues();
-            }}
-            error={props.showPhsVariableValuesError} />
-          <FormHelperText className="helper-text">Example: <i>{INDEX_VARIABLES[variable]['uiValuesExample']}</i></FormHelperText>
+          <Box className={classes.extraIndent}>
+            <TextField
+              fullWidth
+              id="standard-multiline-flexible"
+              label="Enter values (one per line)"
+              multiline
+              rowsMax={20}
+              rows={6}
+              value={variableValues}
+              variant="outlined"
+              disabled={valueOptionRadio === "optionSelect"}
+              onChange={handleChangeValuesField}
+              onBlur={e => {
+                handleChangeValuesField(e);
+                props.validateStep1VariableValues();
+              }}
+              error={props.showPhsVariableValuesError} />
+            <FormHelperText className="helper-text">Example: <i>{INDEX_VARIABLES[variable]['uiValuesExample']}</i></FormHelperText>
+          </Box>
 
           {/* Option to use location values from one or more states */}
           <FormControlLabel value="optionSelect" control={<Radio color={"primary"}/>} label="Use all values from selected states"/>
-          <Autocomplete
-            multiple
-            id="state-autocomplete-id"
-            options={states}
-            disableCloseOnSelect
-            disabled={valueOptionRadio === "optionEnter"}
-            onChange={(event, values) => handleChangeLocationSelect(values, variable)}
-            onBlur={props.validateStep1VariableValues}
-            getOptionLabel={(option) => option.name}
-            renderOption={(option, {selected}) => (
-              <React.Fragment>
-                <Checkbox
-                  color={"primary"}
-                  icon={icon}
-                  checkedIcon={checkedIcon}
-                  style={{marginRight: 8}}
-                  checked={selected}
+          <Box className={classes.extraIndent}>
+            <Autocomplete
+              multiple
+              id="state-autocomplete-id"
+              options={states}
+              disableCloseOnSelect
+              disabled={valueOptionRadio === "optionEnter"}
+              onChange={(event, values) => handleChangeLocationSelect(values, variable)}
+              onBlur={props.validateStep1VariableValues}
+              getOptionLabel={(option) => option.name}
+              renderOption={(option, {selected}) => (
+                <React.Fragment>
+                  <Checkbox
+                    color={"primary"}
+                    icon={icon}
+                    checkedIcon={checkedIcon}
+                    style={{marginRight: 8}}
+                    checked={selected}
+                  />
+                  {option.name}
+                </React.Fragment>
+              )}
+              renderInput={(params) => (
+                <TextField {...params}
+                  error={props.showLocationsError}
+                  variant="outlined" 
+                  label="Select states..." 
+                  placeholder=""
                 />
-                {option.name}
-              </React.Fragment>
-            )}
-            renderInput={(params) => (
-              <TextField {...params}
-                error={props.showLocationsError}
-                variant="outlined" 
-                label="Select states..." 
-                placeholder=""
-              />
-            )}
-          />
+              )}
+            />
+          </Box>
         </RadioGroup>
       </FormControl>
     </React.Fragment>
